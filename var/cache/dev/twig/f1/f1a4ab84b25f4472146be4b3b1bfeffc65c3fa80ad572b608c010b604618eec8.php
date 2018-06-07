0e519d280e2adbf4b0d56445949d3fb9b33b71da
christophe lupatelli <lupat01@yahoo.fr>
2018-06-07 17:07:01 +0200

    fix url img prod

---
+++ var/cache/dev/twig/f1/f1a4ab84b25f4472146be4b3b1bfeffc65c3fa80ad572b608c010b604618eec8.php
@@ -0,0 +1,200 @@
+<?php
+
+/* cloth/show.html.twig */
+class __TwigTemplate_5dfbed2284ca096a2b26abfe9f321f2d106df5d177ba2516837cfe6a55936793 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("base.html.twig", "cloth/show.html.twig", 1);
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'stylesheets' => array($this, 'block_stylesheets'),
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "cloth/show.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "cloth/show.html.twig"));
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
+
+        echo twig_escape_filter($this->env, $this->getAttribute(($context["cloth"] ?? $this->getContext($context, "cloth")), "name", array()), "html", null, true);
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 5
+    public function block_stylesheets($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        // line 6
+        echo "<link rel=\"stylesheet\" href=\"";
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("css/cloth/show.css"), "html", null, true);
+        echo "\">
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 9
+    public function block_body($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        // line 10
+        echo "
+
+<h2>";
+        // line 12
+        echo twig_escape_filter($this->env, $this->getAttribute(($context["cloth"] ?? $this->getContext($context, "cloth")), "name", array()), "html", null, true);
+        echo "</h2>
+<div class=\"container\">
+\t<div class=\"row justify-content-lg-around\">
+\t\t<div class=\"description \">
+\t\t\t<p><span>Couleur:</span> ";
+        // line 16
+        echo twig_escape_filter($this->env, $this->getAttribute(($context["cloth"] ?? $this->getContext($context, "cloth")), "color", array()), "html", null, true);
+        echo "</p>
+\t\t\t<p><span>Saison:</span> ";
+        // line 17
+        echo twig_escape_filter($this->env, $this->getAttribute(($context["cloth"] ?? $this->getContext($context, "cloth")), "season", array()), "html", null, true);
+        echo "</p>
+\t\t\t<p><span>Température:</span> ";
+        // line 18
+        echo twig_escape_filter($this->env, $this->getAttribute(($context["cloth"] ?? $this->getContext($context, "cloth")), "temperature", array()), "html", null, true);
+        echo " °C</p>
+\t\t\t<p><span>Type:</span> ";
+        // line 19
+        echo twig_escape_filter($this->env, $this->getAttribute(($context["cloth"] ?? $this->getContext($context, "cloth")), "part", array()), "html", null, true);
+        echo "</p>
+\t\t</div>
+
+\t\t<div class=\"image\">
+\t\t\t<img class=\"img\" src=\"";
+        // line 23
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl(("/PimpMyOutfit/web/img/" . $this->getAttribute(($context["cloth"] ?? $this->getContext($context, "cloth")), "image", array()))), "html", null, true);
+        echo "\" alt=\"photo du vêtement\">
+\t\t</div>
+
+\t</div>
+
+\t<a href=\"";
+        // line 28
+        echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("dressing_index");
+        echo "\"><i class=\"text-center fas fa-undo-alt my-5\">Retour</i></a>
+</div>
+
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "cloth/show.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  129 => 28,  121 => 23,  114 => 19,  110 => 18,  106 => 17,  102 => 16,  95 => 12,  91 => 10,  82 => 9,  69 => 6,  60 => 5,  42 => 3,  11 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{% extends 'base.html.twig' %}
+
+{% block title %}{{ cloth.name }}{% endblock %}
+
+{% block stylesheets %}
+<link rel=\"stylesheet\" href=\"{{asset('css/cloth/show.css')}}\">
+{% endblock %}
+
+{% block body %}
+
+
+<h2>{{ cloth.name }}</h2>
+<div class=\"container\">
+\t<div class=\"row justify-content-lg-around\">
+\t\t<div class=\"description \">
+\t\t\t<p><span>Couleur:</span> {{cloth.color}}</p>
+\t\t\t<p><span>Saison:</span> {{cloth.season}}</p>
+\t\t\t<p><span>Température:</span> {{cloth.temperature}} °C</p>
+\t\t\t<p><span>Type:</span> {{cloth.part}}</p>
+\t\t</div>
+
+\t\t<div class=\"image\">
+\t\t\t<img class=\"img\" src=\"{{asset('/PimpMyOutfit/web/img/' ~ cloth.image)}}\" alt=\"photo du vêtement\">
+\t\t</div>
+
+\t</div>
+
+\t<a href=\"{{path('dressing_index')}}\"><i class=\"text-center fas fa-undo-alt my-5\">Retour</i></a>
+</div>
+
+{% endblock %}
+", "cloth/show.html.twig", "/var/www/html/perso/PMO_v1/PimpMyOutfit/app/Resources/views/cloth/show.html.twig");
+    }
+}
